01a165453c36aaa0b5cefe7c357751d2c9ab6def
chatfly <oiamigopao@outlook.com>
2025-09-24 17:57:27 -0300

    fix: Cria uma Bean para configuração

---
+++ src/main/java/com/EduManager/infrastructure/beans/BeanConfiguration.java
@@ -0,0 +1,25 @@
+package com.EduManager.infrastructure.beans;
+
+import com.EduManager.core.gateway.AlunoGateway;
+import com.EduManager.core.usecases.CadastrarAlunoUsecase;
+import com.EduManager.core.usecases.CadastrarAlunoUsecaseImpl;
+import com.EduManager.infrastructure.gateway.AlunoRepositoryGateway;
+import com.EduManager.infrastructure.mapper.AlunoEntityMapper;
+import com.EduManager.infrastructure.persistence.AlunoRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    public AlunoGateway alunoGateway(AlunoRepository alunoRepository, AlunoEntityMapper alunoEntityMapper) {
+        return new AlunoRepositoryGateway(alunoRepository, alunoEntityMapper);
+    }
+
+    @Bean
+    public CadastrarAlunoUsecase cadastrarAluno(AlunoGateway alunoGateway) {
+        return new CadastrarAlunoUsecaseImpl(alunoGateway);
+    }
+
+}
