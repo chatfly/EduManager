ff0211dc27bec0b165a5a776bc587f3e70d6ddf8
chatfly <oiamigopao@outlook.com>
2025-09-24 16:31:42 -0300

    feat: Adiciona os arquivos da estrutura inicial do projeto.

---
+++ src/test/java/com/EduManager/EduManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EduManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
