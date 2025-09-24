682f42e129f3c9f411fc85b9c47c4ff477d351e0
chatfly <oiamigopao@outlook.com>
2025-09-24 17:33:32 -0300

    fix: Conserta o equívoco de inverter os Mappers

--- src/main/java/com/EduManager/infrastructure/mapper/AlunoDtoMapper.java
+++ src/main/java/com/EduManager/infrastructure/mapper/AlunoDtoMapper.java
@@ -1,28 +1,29 @@
 package com.EduManager.infrastructure.mapper;
 
 import com.EduManager.core.entities.Aluno;
+import com.EduManager.infrastructure.dtos.AlunoDTO;
 import com.EduManager.infrastructure.persistence.AlunoEntity;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AlunoDtoMapper {
-
-    public AlunoEntity map(Aluno aluno) {
-        return new AlunoEntity(
-            aluno.id(),
-            aluno.nome(),
-            aluno.cpf(),
-            aluno.dataNascimento()
-        );
-    }
-
-    public Aluno map(AlunoEntity alunoEntity) {
+    public Aluno map(AlunoDTO dto)
+    {
         return new Aluno(
-            alunoEntity.getId(),
-            alunoEntity.getNome(),
-            alunoEntity.getCpf(),
-            alunoEntity.getDataNascimento()
+                dto.getId(),
+                dto.getNome(),
+                dto.getCpf(),
+                dto.getDataNascimento()
         );
     }
 
+    public AlunoDTO map(Aluno aluno)
+    {
+        return new AlunoDTO(
+                aluno.id(),
+                aluno.nome(),
+                aluno.cpf(),
+                aluno.dataNascimento()
+        );
+    }
 }
--- src/main/java/com/EduManager/infrastructure/mapper/AlunoEntityMapper.java
+++ src/main/java/com/EduManager/infrastructure/mapper/AlunoEntityMapper.java
@@ -2,28 +2,28 @@ package com.EduManager.infrastructure.mapper;
 
 import com.EduManager.core.entities.Aluno;
 import com.EduManager.infrastructure.dtos.AlunoDTO;
+import com.EduManager.infrastructure.persistence.AlunoEntity;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AlunoEntityMapper {
 
-    public Aluno map(AlunoDTO dto)
-    {
-        return new Aluno(
-            dto.getId(),
-            dto.getNome(),
-            dto.getCpf(),
-            dto.getDataNascimento()
+    public AlunoEntity map(Aluno aluno) {
+        return new AlunoEntity(
+                aluno.id(),
+                aluno.nome(),
+                aluno.cpf(),
+                aluno.dataNascimento()
         );
     }
 
-    public AlunoDTO map(Aluno aluno)
-    {
-        return new AlunoDTO(
-            aluno.id(),
-            aluno.nome(),
-            aluno.cpf(),
-            aluno.dataNascimento()
+    public Aluno map(AlunoEntity alunoEntity) {
+        return new Aluno(
+                alunoEntity.getId(),
+                alunoEntity.getNome(),
+                alunoEntity.getCpf(),
+                alunoEntity.getDataNascimento()
         );
     }
+
 }
